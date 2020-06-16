e73bd24c3e8f620aade2d4f9dede25f8d59f53cc
Donovan K <donovankutzer@gmail.com>
2020-06-15 17:55:33 -0700

    Refactored code

--- panorama.py
+++ panorama.py
@@ -2,6 +2,7 @@ import numpy as np
 from matplotlib import pyplot as plt
 import cv2
 import os
+
 cv2.__version__
 
 # Get the name of all files in images dir to add to list
@@ -10,13 +11,14 @@ images: list = []
 # To stitch all images together
 stitcher: cv2.Stitcher = cv2.Stitcher_create()
 
+
 def main():
     # Add all images in dir to cv2 and add to list
     for file in files:
         print('images/' + file)
         image = cv2.imread('images/' + file)
         images.append(image)
-    
+
     row, col = 1, len(images)
     fig, axs = plt.subplots(row, col, figsize=(10, 5))
     fig.tight_layout()
@@ -36,7 +38,7 @@ def main():
 
     # Continues if stitching is successful
     if status == 0:
-         # Adds stitched image to plot
+        # Adds stitched image to plot
         axs[0].imshow(cv2.cvtColor(stitched, cv2.COLOR_BGR2RGB))
         axs[0].set_title('Stitched')
 
@@ -50,12 +52,11 @@ def main():
 
         cv2.imwrite('stitched.jpg', stitched)
         cv2.imwrite('cropped.jpg', cropped)
-        
-       
+
     else:
         print('Stitching failed (' + str(status) + ')')
 
- 
+
 def crop_img(image):
     # Convert image to grayscale for contour finding
     gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
@@ -66,6 +67,38 @@ def crop_img(image):
     contours, hierarchy = cv2.findContours(thresh, cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)
 
     # Finds largest contour, AKA the entire image outline
+    best = find_largest_contour(contours)
+
+    # Creates a 4 point polygon
+    outer = find_outer_points(best)
+
+    if outer is None:
+        print("Cannot find outer coordinates")
+        return
+
+    print(outer)
+
+    # Gathers actual x, y coordinates for cropping
+
+    # Sort by x value
+    outer = sorted(outer, key=lambda x: x[0][0])
+    x1 = np.maximum(outer[0][0][0], outer[1][0][0])
+    x2 = np.minimum(outer[2][0][0], outer[3][0][0])
+
+    # Sort by y value
+    outer = sorted(outer, key=lambda x: x[0][1])
+    y1 = np.maximum(outer[0][0][1], outer[1][0][1])
+    y2 = np.minimum(outer[2][0][1], outer[3][0][1])
+
+    # Crops image with points found above
+    print("%d:%d , %d:%d" % (y1, y2, x1, x2))
+    cropped = image[y1: y2, x1: x2]
+
+    return cropped
+
+
+# Returns largest contour
+def find_largest_contour(contours):
     max_area = 0
     best = None
     for c in contours:
@@ -73,41 +106,25 @@ def crop_img(image):
         if cur > max_area:
             max_area = cur
             best = c
+    return best
 
-     # Creates a 4 point polygon
+
+# Attempts to approximate given contour down to 4 points
+def find_outer_points(contour):
     i = 0.05
-    approx = cv2.approxPolyDP(best, i * cv2.arcLength(c, True), True)
+    approx = cv2.approxPolyDP(contour, i * cv2.arcLength(contour, True), True)
     previous = ""
     while len(approx) != 4:
         if len(approx) < 4:
-            if previous == "increment": return
+            if previous == "increment": return None
             i -= 0.01
             previous = "decrement"
-        elif len(approx) >4:
-            if previous == "decrement": return
+        elif len(approx) > 4:
+            if previous == "decrement": return None
             i += 0.01
             previous = "increment"
-        approx = cv2.approxPolyDP(best, i * cv2.arcLength(c, True), True)
-
-
-    approx = sorted(approx, key=lambda x: x[0][0])
-
-    print(approx)
-
-    # Finds the inner points, removing all black outer area
-
-    x1 = np.maximum(approx[0][0][0], approx[1][0][0])
-    x2 = np.minimum(approx[2][0][0], approx[3][0][0])
-
-    approx = sorted(approx, key=lambda x: x[0][1])
-    y1 = np.maximum(approx[0][0][1], approx[1][0][1])
-    y2 = np.minimum(approx[2][0][1], approx[3][0][1])
-
-    # Crops image with points found above
-    print("%d:%d , %d:%d" %(y1,y2,x1,x2))
-    cropped = image[y1: y2, x1: x2]
-
-    return cropped
+        approx = cv2.approxPolyDP(contour, i * cv2.arcLength(contour, True), True)
+    return approx
 
 
 if __name__ == '__main__':
